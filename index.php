1ebc9cf1730d97b357df8929556e1065245c8254
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-28 21:25:49 +0200

    created Space/Variables namespace

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 
 require_once 'data.php';
+require_once 'variables.php';
 
+//calling namespace form data.php
 use function Space\Data\text;
 
+//calling class from namespace Space\Variables
+use Space\Variables\all_variables;
+
+//calling function from Space\data\text namespace function
 text();
+
+//calling function from Space\Variables\all_variables; class namespace
+echo all_variables::more();
+
+//creating class container from namespace
+$my_vars = new Space\Variables\all_variables;
+
+//calling function from class $my_vars
+echo $my_vars->vars_value();
+
---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Space\Variables;
+
+class all_variables
+    {
+    
+        function vars_value(){
+        $val = 'value';
+        echo "<br>", $val;
+        }
+    
+        function more(){
+            $ten = 10;
+            $twenty = 20;
+            echo "<br>", $ten * $twenty;
+        }
+    
+    }
