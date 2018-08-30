81e11bac662f51d9a01d211d52b3caa2971fec20
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-30 17:59:30 +0200

    branch init

--- constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace Mind_const\Values;
-
-define("CNS", "CNS is an abbreviation of the Central Nervous System");
--- data.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Mind_lobes\Data;
-
-function write_lobes(){
-    echo "Frontal lobe, parietal lobe, temporal lobe, occipital lobe";
-}
